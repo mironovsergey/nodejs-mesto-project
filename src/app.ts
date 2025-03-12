5cf9ed696ff0d8f4b984da9f2d60ec6742a2b1f4
Sergey Mironov <sergeymironov@protonmail.com>
2025-03-12 09:40:28 +0300

    Обработка 404 и ограничение запросов

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import mongoose, { Types } from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users';
 import cardRoutes from './routes/cards';
+import HTTP_STATUS from './constants/http';
 
 dotenv.config();
 
@@ -11,8 +13,17 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: { message: 'Превышен лимит запросов' },
+});
+
 mongoose.connect(MONGO_URI);
 
+app.use(limiter);
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -22,5 +33,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
+
+app.use((req: Request, res: Response) => {
+  res.status(HTTP_STATUS.NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT);
